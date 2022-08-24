1be7600a609aa4ada8d2518442b9732f9ed3f410
trench009 <amentrench2@gmail.com>
2022-08-24 22:26:00 +0500

    add these

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Mainportal/Header/header/header.component';
+import { HomeComponent } from './Mainportal/Home/home/home.component';
+import { FooterComponent } from './Mainportal/Footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
