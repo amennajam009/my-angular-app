c8cc0b06cef5a1e22e42519b8d71b28aced05c61
trench009 <amentrench2@gmail.com>
2022-08-25 12:39:05 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Mainportal/Home/home/home.component';
+import { LinkComponent } from './Mainportal/link/link/link.component';
 
 const routes: Routes = [
   {path:'' , component:HomeComponent},
   {path:'home', component:HomeComponent},
+  {path:'link', component:LinkComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Mainportal/Header/header/header.component';
 import { HomeComponent } from './Mainportal/Home/home/home.component';
 import { FooterComponent } from './Mainportal/Footer/footer/footer.component';
+import { LinkComponent } from './Mainportal/link/link/link.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FooterComponent } from './Mainportal/Footer/footer/footer.component';
     HeaderComponent,
     HomeComponent,
     FooterComponent,
+    LinkComponent,
     
   ],
   imports: [
