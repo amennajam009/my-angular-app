17488be7e417734096d298af816e209e407da8ed
trench009 <amentrench2@gmail.com>
2022-09-04 12:27:32 +0500

    add theses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionComponent } from './Mainportal/collection/collection/collection.component';
+import { HeaderComponent } from './Mainportal/Header/header/header.component';
 import { HomeComponent } from './Mainportal/Home/home/home.component';
 import { LoginComponent } from './Mainportal/login/login/login.component';
 import { PagenotfoundComponent } from './Mainportal/pagenotfound/pagenotfound/pagenotfound.component';
@@ -11,6 +12,7 @@ import { UnstitchedComponent } from './Mainportal/Unstitched/unstitched/unstitch
 
 const routes: Routes = [
   {path:'' , component:HomeComponent},
+  {path:'header', component:HeaderComponent},
   {path:'home', component:HomeComponent},
   {path:'shop', component:ShopComponent},
   {path:'Unstitched',component:UnstitchedComponent},
