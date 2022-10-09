f12437b5227709143d261c31d8a9171e053e39e4
trench009 <amennajam7@gmail.com>
2022-10-09 20:20:57 +0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Mainportal/Header/header/header.component';
 import { HomeComponent } from './Mainportal/Home/home/home.component';
-
-
 import { FooterComponent } from './Mainportal/footer/footer/footer.component';
 import { ShopComponent } from './Mainportal/shop/shop/shop.component';
 import { UnstitchedComponent } from './Mainportal/Unstitched/unstitched/unstitched.component';
